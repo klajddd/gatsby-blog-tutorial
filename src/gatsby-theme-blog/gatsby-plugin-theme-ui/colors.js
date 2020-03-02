167834092a994e2a8f2a78d323350a84f57427b0
Klajd Karaj <klajdkaraj14@gmail.com>
2020-03-02 01:20:33 +0000

    Update theme ui colors.

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -7,19 +7,28 @@ import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/col
  * to go from default purple to a blue theme
  */
 
-// const darkBlue = `#007acc`
-// const lightBlue = `#66E0FF`
-// const blueGray = `#282c35`
+const darkBlue = `#007acc`
+const lightBlue = `#66E0FF`
+const blueGray = `#282c35`
+const purple60 = `#663399`
+const purple30 = `#D9BAE8`
+const grey90 = `#232129`
+const black80 = `#1B1F23`
+const white = `#fff`
+const lightWhite = `rgba(255, 255, 255, 0.86)`
+const opaqueLightYellow = `rgba(255, 229, 100, 0.2)`
+const opaqueLightWhite = `hsla(0, 0%, 100%, 0.2)`
+const lightGray = `hsla(0, 0%, 0%, 0.2)`
 
 export default merge(defaultThemeColors, {
-  // text: blueGray,
-  // primary: darkBlue,
-  // heading: blueGray,
-  // modes: {
-  //   dark: {
-  //     background: blueGray,
-  //     primary: lightBlue,
-  //     highlight: lightBlue,
-  //   },
-  // },
+  text: blueGray,
+  primary: purple60,
+  heading: blueGray,
+  modes: {
+    dark: {
+      background: blueGray,
+      primary: lightBlue,
+      highlight: lightBlue,
+    },
+  },
 })
